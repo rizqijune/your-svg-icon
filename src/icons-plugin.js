9018481d43ee5750fc120dfd372dc3218af3a3b7
rizqijune <cancerrizqi@gmai.com>
2024-10-19 22:19:45 +0700

    Update code

--- src/icons-plugin.js
+++ src/icons-plugin.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function iconPlugin({ addUtilities }, options = {}) {
-  const iconsDir = options.iconsDir || path.resolve(__dirname, 'icons');
+  const iconsDir = options.iconsDir || path.resolve(__dirname, 'icons'); // Default to './src/icons'
+  
+  if (!fs.existsSync(iconsDir)) {
+    console.warn(`Warning: Icons directory ${iconsDir} does not exist.`);
+    return;
+  }
+
   const svgFiles = fs.readdirSync(iconsDir).filter(file => file.endsWith('.svg'));
 
   const iconUtilities = {};
@@ -10,7 +16,7 @@ module.exports = function iconPlugin({ addUtilities }, options = {}) {
   svgFiles.forEach(file => {
     const iconName = path.basename(file, '.svg');
     iconUtilities[`.icon-${iconName}`] = {
-      backgroundImage: `url('./icons/${file}')`,
+      backgroundImage: `url('./icons/${file}')`,  // Ensure this path is correct
       backgroundSize: 'contain',
       backgroundRepeat: 'no-repeat',
       display: 'inline-block',
