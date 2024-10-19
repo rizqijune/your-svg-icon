a94431073858d96cb1fb65112bb51e45e410ca11
rizqijune <cancerrizqi@gmai.com>
2024-10-19 22:31:32 +0700

    Changes

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const YSIcons = require('./src/icons-plugin');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = YSIcons;
+module.exports = function iconPlugin({ addUtilities }, options = {}) {
+  const iconsDir = options.iconsDir || path.resolve(__dirname, 'src/icons'); // Default to './src/icons'
+  
+  if (!fs.existsSync(iconsDir)) {
+    console.warn(`Warning: Icons directory ${iconsDir} does not exist.`);
+    return;
+  }
+
+  const svgFiles = fs.readdirSync(iconsDir).filter(file => file.endsWith('.svg'));
+
+  const iconUtilities = {};
+
+  svgFiles.forEach(file => {
+    const iconName = path.basename(file, '.svg');
+    iconUtilities[`.icon-${iconName}`] = {
+      backgroundImage: `url('./icons/${file}')`,  // Ensure this path is correct
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+      display: 'inline-block',
+    };
+  });
+
+  addUtilities(iconUtilities, ['responsive', 'hover']);
+};
